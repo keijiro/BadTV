628a62acc9c4fc5e0163df8686ea5696076c1bb6
Keijiro Takahashi <keijiro@gmail.com>
2025-03-03 15:54:26 +0900

    tweaks

--- Assets/Corruptor.hlsl
+++ Assets/Corruptor.hlsl
@@ -1,14 +1,21 @@
-void CorruptorMain_float(UnityTexture2D Source, float2 UV, out float3 Output)
+#include "Packages/jp.keijiro.noiseshader/Shader/Noise1D.hlsl"
+
+void CorruptorMain_float
+  (UnityTexture2D Source,
+   float2 UV,
+   UnityTexture3D LUT,
+   float Distortion,
+   out float3 Output)
 {
-    float2 grid = UV * float2(300, 100) + float2(0, _Time.y * 200);
+    bool mask = all(frac(UV * float2(128, 72)) > 0.5);
 
-    float hoffs = 0;
+    float2 p = (UV - 0.5) * 2;
+    float scale = 1 - Distortion * (1 - dot(p, p));
+    UV = p * scale * 0.5 + 0.5;
 
-    float3 color = LinearToSRGB(tex2D(Source, UV + float2(0, hoffs)).rgb);
+    float3 color = LinearToSRGB(tex2D(Source, UV).rgb);
 
-    float2 wave = abs(sin(grid * PI));
-
-    color *= wave.x * wave.y;
+    color = tex3D(LUT, color).rgb * mask;
 
     Output = SRGBToLinear(color);
 }
