ab444aee733e5448f647929b61a38fa0b9433e61
Keijiro Takahashi <keijiro@gmail.com>
2025-03-04 16:47:46 +0900

    Projector

--- Assets/Corruptor.hlsl
+++ Assets/Corruptor.hlsl
@@ -7,15 +7,13 @@ void CorruptorMain_float
    float Distortion,
    out float3 Output)
 {
-    bool mask = all(frac(UV * float2(128, 72)) > 0.5);
-
     float2 p = (UV - 0.5) * 2;
     float scale = 1 - Distortion * (1 - dot(p, p));
     UV = p * scale * 0.5 + 0.5;
 
     float3 color = LinearToSRGB(tex2D(Source, UV).rgb);
 
-    color = tex3D(LUT, color).rgb * mask;
+    color = tex3D(LUT, color).rgb;
 
     Output = SRGBToLinear(color);
 }
